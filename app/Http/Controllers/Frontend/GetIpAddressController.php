cc0f9c1cbaa1b94f6d3db955781247d424e3b621
Carlo Bactol <you@example.com>
2022-10-28 21:43:53 +0800

    add ipaddress

--- app/Http/Controllers/Frontend/GetIpAddressController.php
+++ app/Http/Controllers/Frontend/GetIpAddressController.php
@@ -9,7 +9,21 @@ class GetIpAddressController extends Controller
 {
     public function index(Request $request)
     {
-        $ip = $request->getClientIp();
-        dd($ip);
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if (isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if (isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if (isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
     }
 }
