cdae2f582799345cb43ecc4970088761532c1ee3
Carlo Bactol <you@example.com>
2022-10-29 15:04:46 +0800

    first commit

--- app/Http/Controllers/Frontend/GetIpAddressController.php
+++ app/Http/Controllers/Frontend/GetIpAddressController.php
@@ -10,25 +10,28 @@ class GetIpAddressController extends Controller
 {
     public function index(Request $request)
     {
-        $ipaddress = '';
-        if (isset($_SERVER['HTTP_CLIENT_IP']))
-            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        else if (isset($_SERVER['HTTP_X_FORWARDED']))
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
-            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        else if (isset($_SERVER['HTTP_FORWARDED']))
-            $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        else if (isset($_SERVER['REMOTE_ADDR']))
-            $ipaddress = $_SERVER['REMOTE_ADDR'];
-        else
-            $ipaddress = 'UNKNOWN';
+        // $ipaddress = '';
+        // if (isset($_SERVER['HTTP_CLIENT_IP']))
+        //     $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        // else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        //     $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        // else if (isset($_SERVER['HTTP_X_FORWARDED']))
+        //     $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        // else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
+        //     $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        // else if (isset($_SERVER['HTTP_FORWARDED']))
+        //     $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        // else if (isset($_SERVER['REMOTE_ADDR']))
+        //     $ipaddress = $_SERVER['REMOTE_ADDR'];
+        // else
+        //     $ipaddress = 'UNKNOWN';
 
 
-        $ip = $ipaddress;
-        $data = Location::get($ip);
-        return view('frontend.mylocation', compact('data'));
+        // $ip = $ipaddress;
+        // $data = Location::get($ip);
+        // return view('frontend.mylocation', compact('data'));
+        $locationData = Location::get('https://' . $request->ip()); // https or http according to your necessary.
+
+        return view('frontend.mylocation', compact('locationData'));
     }
 }
--- resources/views/frontend/mylocation.blade.php
+++ resources/views/frontend/mylocation.blade.php
@@ -1,1 +1,1 @@
-{{print_r($data) }}
+{{dd($locationData) }}
