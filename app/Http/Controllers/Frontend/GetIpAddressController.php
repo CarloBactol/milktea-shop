e90e896251ee606028f990bdda698f35892bf993
Carlo Bactol <you@example.com>
2022-10-29 14:55:27 +0800

    changes config ip

--- app/Http/Controllers/Frontend/GetIpAddressController.php
+++ app/Http/Controllers/Frontend/GetIpAddressController.php
@@ -10,26 +10,25 @@ class GetIpAddressController extends Controller
 {
     public function index(Request $request)
     {
-        // $ipaddress = '';
-        // if (isset($_SERVER['HTTP_CLIENT_IP']))
-        //     $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        // else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        //     $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        // else if (isset($_SERVER['HTTP_X_FORWARDED']))
-        //     $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        // else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
-        //     $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        // else if (isset($_SERVER['HTTP_FORWARDED']))
-        //     $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        // else if (isset($_SERVER['REMOTE_ADDR']))
-        //     $ipaddress = $_SERVER['REMOTE_ADDR'];
-        // else
-        //     $ipaddress = 'UNKNOWN';
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if (isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if (isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if (isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
 
 
-        // $ip = $ipaddress;
-        // $data = Location::get($ip);
-        // return $data;
-        return view('frontend.mylocation');
+        $ip = $ipaddress;
+        $data = Location::get($ip);
+        return view('frontend.mylocation', compact('data'));
     }
 }
